831b7b6cdd9c3856c80a4e370b757c60f1cfa799
solomondworkin <solomon.dworkin@gmail.com>
2020-10-13 23:57:57 -0500

    goddammitt

--- computersystems/project0.py
+++ computersystems/project0.py
@@ -1,7 +1,7 @@
 # get file name
 name = input("What is the name of your file?")
 
-#get directory
+# get directory
 directory = input("What is the directory called?")
 
 # input directorylocation/<filename>.in
@@ -10,10 +10,29 @@ directory = input("What is the directory called?")
 
 with open(directory+"/"+name+"a", "w") as output, open(directory+"/"+name,"r") as data:
     lines = data.readlines()
+    comment = False
+    # for loop iterates through each line in document
     for line in lines:
         l = line.split("//")
         l[0] = l[0].replace(" ", "")
         if len(l) > 1:
             l[0] += "\n"
-        if l[0].rstrip():
-            output.write(l[0])
+        if len(l) == 0:
+            l[0] = l[0].replace("\n","")
+        character = 0
+        begin_comment = "/*"
+        end_comment = "*/"
+        # while loop iterates through each character in each line
+        while character < len(l[0]):
+            # if it encounters the beginning of a comment, changes comment value to true
+            if l[0][character:character+2] == begin_comment and comment == False:
+                comment = True
+                character += 1
+            # if it encounters the end of a comment, changes the comment value to false
+            elif l[0][character:character+2] == end_comment and comment == True:
+                comment = False
+                character += 1
+            # if it encounters a character that is not white space, it writes the character
+            elif comment == False and character != "\n" and character != "\t":
+                output.write(l[0][character])
+            character +=1
