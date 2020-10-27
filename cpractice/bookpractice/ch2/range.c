70c03c7324366af51bfb6c132b132c76c9859d23
solomondworkin <solomon.dworkin@gmail.com>
2020-10-27 15:57:11 -0500

    started exercises for chapter 3 of K&R

---
+++ cpractice/bookpractice/ch2/range.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h>
+
+// compute the sizes of char, short, int, and long variables.
+int main () {
+
+    printf("The maximum numeric value of type char is: %d\n", CHAR_MAX);
+    printf("The minimum numeric value of type char is: %d\n", CHAR_MIN);
+
+    printf("The maximum numeric value of type int is: %d\n", INT_MAX);
+    printf("The minimum numeric value of type int is: %d\n", INT_MIN);
+
+    printf("The maximum numeric value of type char is: %d\n", CHAR_MIN);
+    printf("The minimum numeric value of type char is: %d\n", CHAR_MIN);
+
+}
---
+++ cpractice/bookpractice/ch3/exercise1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+
+int main () {
+    
+
+}
+
+// Find x in v[0] <= v[1] <= ... <= v[n-1]
+int binsearch (int x, int v[], int n) {
+
+    int low, high, mid;
+
+    low = 0;
+    high = n-1;
+    while (low <= high) {
+        mid = (low+high) / 2;
+        if (x < v[mid])
+            high = mid - 1;
+        else if (x > v[mid])
+            low = mid+1;
+        else
+            return mid;
